184d1c0d6c7c4ca79ebbea56d3eccb0864ed695f
Dushyant Deshmukh <dushyant.deshmukh@hp.com>
2020-01-24 11:58:19 -0500

    first commit

---
+++ src/main/java/com/dushdesh/chucknorrisjokesapp/services/ChuckNorrisJoke.java
@@ -0,0 +1,18 @@
+package com.dushdesh.chucknorrisjokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChuckNorrisJoke implements JokeService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public ChuckNorrisJoke() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
--- src/main/java/com/dushdesh/chucknorrisjokesapp/services/JokeService.java
+++ src/main/java/com/dushdesh/chucknorrisjokesapp/services/JokeService.java
@@ -3,10 +3,6 @@ package com.dushdesh.chucknorrisjokesapp.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JokeService {
-
-    public String getChuckNorrisJoke() {
-        return guru.springframework.norris.chuck.ChuckNorrisQuotes.getRandomQuote();
-
-    }
+public interface JokeService {
+    String getJoke();
 }
